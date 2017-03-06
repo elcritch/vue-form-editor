1970290a87aa18d0d5b58d83f7517bd55177a51a
Jaremy Creechley <creechley@gmail.com>
2017-03-06 13:41:00 -0700

    Panel type.

--- src/formCreator.vue
+++ src/formCreator.vue
@@ -1,7 +1,7 @@
 <template lang="html">
-  <div class="row">
+  <div class="media">
+    <div class="media-left">
 
-    <div class="col-md-6  col-sm-4">
       <div class="box">
         <h2>Vue Form Creator</h2>
         <span class="icon is-medium">
@@ -36,13 +36,82 @@
       </div>
     </div>
 
-    <div class="col-md-6 col-sm-8 ">
-      <div class="box">
+    <div class="media-content ">
+      <nav class="panel ">
+        <p class="panel-heading">
+          Repositories
+        </p>
 
-        <h4 >In UiPopover (creates a dropdown)</h4>
+        <div class="panel-block">
 
+          <label class="label">Name</label>
+          <p class="control">
+            <input class="input" type="text" placeholder="Text input">
+          </p>
+        </div>
 
-      </div>
+        <div class="panel-block">
+          <label class="label">Username</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+            <i class="fa fa-check"></i>
+            <span class="help is-success">This username is available</span>
+          </p>
+        </div>
+
+        <div class="panel-block">
+          <label class="label">Email</label>
+          <p class="control has-icon has-icon-right">
+            <input class="input is-danger" type="text" placeholder="Email input" value="hello@">
+            <i class="fa fa-warning"></i>
+            <span class="help is-danger">This email is invalid</span>
+          </p>
+        </div>
+
+        <div class="panel-block">
+          <label class="label">Subject</label>
+          <p class="control">
+            <span class="select">
+              <select>
+                <option>Select dropdown</option>
+                <option>With options</option>
+              </select>
+            </span>
+          </p>
+        </div>
+
+        <div class="panel-block">
+          <label class="label">Message</label>
+          <p class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+          </p>
+          <p class="control">
+            <label class="checkbox">
+              <input type="checkbox">
+              Remember me
+            </label>
+          </p>
+        </div>
+        <div class="panel-block">
+          <p class="control">
+            <label class="radio">
+              <input type="radio" name="question">
+              Yes
+            </label>
+            <label class="radio">
+              <input type="radio" name="question">
+              No
+            </label>
+          </p>
+        </div>
+        <div class="panel-block">
+          <p class="control">
+            <button class="button is-primary">Submit</button>
+            <button class="button is-link">Cancel</button>
+          </p>
+        </div>
+
+      </nav>
     </div>
 
   </div>
